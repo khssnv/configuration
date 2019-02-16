894068afd68aaaa062a75740b3197d77574d8826
Alisher A. Khassanov <a.khssnv@gmail.com>
2019-02-16 19:58:06 +0400

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cp -rf ./.vimrc ~
 cp -rf ./.gitconfig ~
 
 # VIM Vundle
-if [ -e ~/.vim/bundle/Vundle.vim ];
+if [ ! -e ~/.vim/bundle/Vundle.vim ];
 then
   git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
   vim +PluginInstall +qall
