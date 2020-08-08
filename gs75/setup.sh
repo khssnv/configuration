7ffe59be3b85b72e019a7fae4c3a83a792240b13
Alisher A. Khassanov <ak@remyrobotics.com>
2020-08-08 20:32:16 +0200

    Common home configuration

--- gs75/setup.sh
+++ gs75/setup.sh
@@ -1,3 +1,3 @@
 # cp ./home.nix ~/.config/nixpkgs
-ln -s $(pwd)/home.nix ~/.config/nixpkgs/home.nix
+ln -s $(pwd)/../home.nix ~/.config/nixpkgs/home.nix
 echo "https://github.com/rycee/home-manager"
