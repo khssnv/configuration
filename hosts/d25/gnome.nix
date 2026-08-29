0bfcf6f09c82d0c2e1bcfae13890e2383a56fbbb
Alisher A. Khassanov <a.khssnv@gmail.com>
2026-08-29 12:11:50 +0500

    Set automatic suspend delay to 90 minutes on d25

--- hosts/d25/gnome.nix
+++ hosts/d25/gnome.nix
@@ -1,7 +1,12 @@
 { lib, ... }:
 
 {
-  dconf.settings."org/gnome/desktop/session".idle-delay = lib.hm.gvariant.mkUint32 3600;
+  dconf.settings = {
+    "org/gnome/desktop/session".idle-delay = lib.hm.gvariant.mkUint32 3600;
+
+    "org/gnome/settings-daemon/plugins/power".sleep-inactive-ac-timeout =
+      lib.hm.gvariant.mkUint32 5400;
+  };
 
   xdg.configFile."monitors.xml" = {
     force = true;
